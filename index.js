01cb3fcfe60b9d410b7f6dbf223f2fd74b2f6b3d
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2021-12-04 00:19:03 +0700

    authenticate with passport-mongoStore

--- index.js
+++ index.js
@@ -10,90 +10,68 @@ const cookieParser = require("cookie-parser")
 const ejs = require("ejs")
 const expressSession = require("express-session")
 const fileStore = require("session-file-store")(expressSession)
+const mongoStore = require("connect-mongo")
 const morgan = require("morgan")
+require("dotenv").config()
 // user module
 const auth = require("./auth")
 const api = require("./exercise")
 
 // define
 const app = express()
-const port = 3000
 
 // Middleware
 app.use(morgan("dev"))
 app.use("/user", auth())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(
-  express.urlencoded({
-    extended: true,
+  expressSession({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: mongoStore.create({
+      mongoUrl: "mongodb://localhost:27017/auth",
+    }),
   })
 )
-app.use(cookieParser())
-// app.use(
-//   expressSession({
-//     secret: 'secret',
-//     resave: false,
-//     saveUninitialized: false,
-//     store: new fileStore({}),
-//   })
-// )
-app.use((req, res, next) => {
-  next()
-})
 
 app.set("view engine", "ejs")
 
 // Request
-app.get("/", (req, res) => {
-  res.render("index")
-})
-
 app.get("/login", (req, res) => {
   res.render("login")
 })
 
-app.post("/process_login", (req, res) => {
-  console.log(req.body)
-  const { user, password } = req.body
-  if (user === "rama" && password === "ggwp") {
-    const payload = {}
-    const option = {
-      expiresIn: "30s",
-      issuer: "pickurpage.com",
-      audience: user,
-    }
-    const token = jwt.sign(payload, "secretkey", option)
-    res.send(`${token}`)
-  } else {
-    return res.redirect("/login")
+app.post("/login", (req, res) => {
+  const { username, password } = req.body
+  if (username != "admin") return res.send("username not found")
+  if (password != "ggwp") return res.send("wrong password")
+  req.session.username = username
+  res.redirect("/admin")
+})
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) console.log(err)
+    return
+  })
+  res.redirect("/login")
+})
+
+app.get(
+  "/admin",
+  (req, res, next) => {
+    console.log(req.session.username)
+    if (req.session.username != "admin") return res.status(401).send("Unauthorize")
+    next()
+  },
+  (req, res) => {
+    res.send(`Welcome, ${req.session.username}`)
   }
-})
+)
 
-app.get("/user", (req, res) => {
-  res.send(api.getUser())
-  res.end()
-})
-
-app.get("/user/search", (req, res) => {
-  let { name, age } = req.query
-  if (req.query) {
-    res.send(api.getUser(name, age))
-  } else {
-    res.send("Hello people")
-  }
-  res.end()
-})
-
-app.post("/user/add", (req, res) => {
-  let { name, age } = req.body
-  if (Object.keys(req.body).length > 0) {
-    api.addUser(name, age)
-    res.send("User created")
-  } else {
-    res.send("No data to add")
-  }
-  res.end()
-})
-
-app.listen(port, () => {
-  console.log(`listening to http://127.0.0.1:${port}/`)
+app.listen(process.env.PORT, () => {
+  console.log(`listening to http://127.0.0.1:${process.env.PORT}/`)
 })
